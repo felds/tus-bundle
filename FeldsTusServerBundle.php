a21ab93f9bfcb665ee50a08cc64be981c6a2b64d
Felds Liscia <dev@felds.com.br>
2018-04-30 01:10:38 -0300

    fix import

--- FeldsTusServerBundle.php
+++ FeldsTusServerBundle.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace Felds\TusServerBundle;
 
-use Symfony\Component\HttpKernel\Bundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class FeldsTusServerBundle extends Bundle
 {
