9d7f114f733bbc57456d6969ffbb0410d618280e
Felds Liscia <dev@felds.com.br>
2018-05-21 01:45:55 -0300

    add max_size configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,16 +18,22 @@ class Configuration implements ConfigurationInterface
                 ->scalarNode('entity_class')
                     ->isRequired()
                     ->cannotBeEmpty()
-                    ->info("Your entity class. It must extend Felds\\TusServerBundle\\Model\\AbstractUpload.")
+                    ->info("Your entity class. It must extend Felds\\TusServerBundle\\Model\\AbstractUpload")
                     ->example("App\\Entity\\Upload")
                 ->end()
                 ->scalarNode('expiration')
                     ->defaultNull()
                     ->info(
-                        "Default expiration for the upload URL.".
-                        "\nIt can be a number of seconds or a relative date string to be added to the current time."
+                        "Default expiration for the upload URL".
+                        "\nIt can be a number of seconds or a relative date string to be added to the current time"
                     )
                     ->example("1 day")
+                ->end()
+                ->scalarNode('max_size')
+                    ->isRequired()
+                    ->cannotBeEmpty()
+                    ->info("Max size of the file to be uploaded (in bytes)")
+                    ->example("2GB = 1024^3 * 2 = 2147483648")
                 ->end()
             ->end()
         ;
