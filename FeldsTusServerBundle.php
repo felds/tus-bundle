3ebcdb6cbc79d32732a6696e2eeb5099325fc36d
Felds Liscia <dev@felds.com.br>
2019-03-27 12:16:41 -0300

    tidy up

--- FeldsTusServerBundle.php
+++ FeldsTusServerBundle.php
@@ -3,16 +3,8 @@ declare(strict_types=1);
 
 namespace Felds\TusServerBundle;
 
-use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class FeldsTusServerBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        // $namespaces = ['Felds\\TusServerBundle\\Entity'];
-        // $directories = [__DIR__ . '/Entity'];
-        // $container->addCompilerPass(DoctrineOrmMappingsPass::createAnnotationMappingDriver($namespaces, $directories));
-    }
 }
